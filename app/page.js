54cde045078e07c068962a0f29401f336357575c
Adam <awhiles2011@gmail.com>
2025-04-25 13:43:35 -0500

    update page title and description

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default async function Home() {
   console.log(secret);
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-3xl">Secrets....</h1>
+      <h1 className="text-3xl">Secrets about Chris....</h1>
       <ul>
         <li className="mb-5"><span className="font-extrabold text-lg ">Secret 1: </span> {secret.secret}</li>
         <li><span className="font-extrabold text-lg ">Secret 2: </span> {secret.secret2}</li>
